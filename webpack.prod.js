1a0b019e566711a77672c0cd01475070c4fc3ea3
Chris Palka <cpalka87gmail.com>
2022-12-01 00:42:18 -0700

    Modify webpack.prod.js to correclty incorporate env vars

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,6 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const webpack = require('webpack');
 const DIST_DIR = path.join(__dirname, 'public');
-require('dotenv').config();
 
 
 module.exports = merge(common, {
